2b3d168926a981fe49fecd5bcfc218abfc7ddb66
Alex <iseldomused@gmail.com>
2019-03-09 05:13:15 +0300

    TextAnalysis: FrequencyAnalysis and TextGenerator

--- TextAnalysis/FrequencyAnalysisTask.cs
+++ TextAnalysis/FrequencyAnalysisTask.cs
@@ -1,14 +1,99 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TextAnalysis
 {
-    static class FrequencyAnalysisTask
-    {
-        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
-        {
-            var result = new Dictionary<string, string>();
-            //...
-            return result;
-        }
-   }
+	static class FrequencyAnalysisTask
+	{
+		public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+		{
+			var pairs = text
+				.SelectMany(s => s.GetPairs())
+				.Where(pair => pair != null)
+				.GetMostFrequentPairs();
+
+			var triples = text
+				.SelectMany(s => s.GetTriples())
+				.Where(triple => triple != null)
+				.Select(triple => new[] {$"{triple[0]} {triple[1]}", triple[2]})
+				.GetMostFrequentPairs();
+
+			var result = new Dictionary<string, string>();
+			foreach (var pair in pairs)
+			{
+				result[pair.Key] = pair.Val;
+			}
+
+			foreach (var triple in triples)
+			{
+				result[triple.Key] = triple.Val;
+			}
+
+			return result;
+		}
+
+		private static IEnumerable<(string Key, string Val)> GetMostFrequentPairs(this IEnumerable<string[]> pairs)
+		{
+			return pairs
+				.GroupBy(pair => pair[0])
+				.Select(group =>
+					(
+						Key: group.Key,
+						Val: group.GroupBy(pair => pair[1]).GetMostFrequentValue()
+					)
+				);
+		}
+
+		private static string GetMostFrequentValue(this IEnumerable<IGrouping<string, string[]>> groups)
+		{
+			var value = (Count: -1, Val: "");
+			foreach (var group in groups)
+			{
+				if (group.Count() > value.Count)
+				{
+					value.Count = group.Count();
+					value.Val = group.Key;
+				}
+				else if (group.Count() == value.Count && string.CompareOrdinal(group.Key, value.Val) < 0)
+				{
+					value.Count = group.Count();
+					value.Val = group.Key;
+				}
+			}
+
+			return value.Val;
+		}
+
+		private static string[][] GetPairs(this List<string> sentence)
+		{
+			if (sentence.Count < 2)
+			{
+				return new string[0][];
+			}
+
+			var pairs = new string[sentence.Count - 1][];
+			for (int i = 0; i < pairs.Length; i++)
+			{
+				pairs[i] = new[] {sentence[i], sentence[i + 1]};
+			}
+
+			return pairs;
+		}
+
+		private static string[][] GetTriples(this List<string> sentence)
+		{
+			if (sentence.Count < 3)
+			{
+				return new string[0][];
+			}
+
+			var triples = new string[sentence.Count - 2][];
+			for (int i = 0; i < triples.Length; i++)
+			{
+				triples[i] = new[] {sentence[i], sentence[i + 1], sentence[i + 2]};
+			}
+
+			return triples;
+		}
+	}
 }
--- TextAnalysis/TextGeneratorTask.cs
+++ TextAnalysis/TextGeneratorTask.cs
@@ -1,15 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TextAnalysis
 {
     static class TextGeneratorTask
     {
-        public static string ContinuePhrase(
-            Dictionary<string, string> nextWords,
-            string phraseBeginning,
-            int wordsCount)
+        public static string ContinuePhrase(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
         {
-            return phraseBeginning;
+	        var words = phraseBeginning.Split(' ').ToList();
+			for (int i = 0; i < wordsCount; i++)
+			{
+				var next = words.TakeLast(2).ToList().GetNext(nextWords);
+				if (next == null)
+				{
+					break;
+				}
+				words.Add(next);
+			}
+            return string.Join(" ", words);
         }
-    }
+
+	    private static string GetNext(this List<string> phraseBeginning, Dictionary<string, string> nextWords)
+	    {
+		    if (phraseBeginning.Count == 2)
+		    {
+			    var beginnig = string.Join(" ", phraseBeginning);
+			    if (nextWords.ContainsKey(beginnig))
+			    {
+				    return nextWords[beginnig];
+			    }
+
+			    return nextWords.Get(phraseBeginning[1]);
+		    }
+		    if (phraseBeginning.Count == 1)
+		    {
+			    return nextWords.Get(phraseBeginning[0]);
+		    }
+
+		    return null;
+	    }
+
+	    private static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int count) {
+		    return source.Skip(Math.Max(0, source.Count() - count));
+	    }
+
+	    private static TValue Get<TKey, TValue>(this Dictionary<TKey, TValue> source, TKey key) {
+		    if (source.ContainsKey(key))
+		    {
+			    return source[key];
+		    }
+
+		    return default(TValue);
+	    }
+	}
 }
